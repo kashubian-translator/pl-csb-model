43204e1ef1dac768ed9550b2c41cfa3cd5fbf7d4
FilipSzweda <filip.szweda1@gmail.com>
2024-05-27 22:47:46 +0200

    Refactor tokenizer

---
+++ train_model/__main__.py
@@ -0,0 +1,4 @@
+import train_model
+
+if __name__ == "__main__":
+    train_model.main()
---
+++ train_model/train_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from transformers import NllbTokenizer
+from tqdm.auto import tqdm, trange
+import re
+
+def word_tokenize(text: str) -> list:
+    return re.findall('(\w+|[^\w\s])', text)
+
+def prepare_translation_dataset(trans_df: pd.DataFrame, tokenizer: NllbTokenizer) -> pd.DataFrame:
+    dataset = pd.DataFrame()
+
+    dataset['csb_toks'] = trans_df.csb.apply(lambda x: tokenizer.tokenize(str(x)))
+    dataset['pl_toks'] = trans_df.pl.apply(lambda x: tokenizer.tokenize(str(x)))
+
+    dataset['csb_words'] = trans_df.csb.apply(lambda x: word_tokenize(str(x)))
+    dataset['pl_words'] = trans_df.pl.apply(lambda x: word_tokenize(str(x)))
+
+    return dataset
+
+def main() -> None:
+    data_path = "../pl-csb-data/data/train.tsv"
+    trans_df = pd.read_csv(data_path, sep="\t")
+
+    tokenizer = NllbTokenizer.from_pretrained('facebook/nllb-200-distilled-600M')
+
+    dataset = prepare_translation_dataset(trans_df, tokenizer)
