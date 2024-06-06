0a16418bbd20657029cc0b82343ef01a3c5ea9ee
FilipSzweda <filip.szweda1@gmail.com>
2024-06-06 18:20:16 +0200

    Fix data loading

--- pl-csb-model/train/data_loader.py
+++ pl-csb-model/train/data_loader.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def load_train() -> pd.DataFrame:
     data_path = "../pl-csb-data/data/train.tsv"
-    return pd.read_csv(data_path, sep="\t")
+    return pd.read_csv(data_path, sep="\t", index_col=0)
