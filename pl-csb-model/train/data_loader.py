178a840c652d9f863b9e8ca73273c2bdee935b70
Filip Szweda <filip.szweda1@gmail.com>
2024-09-18 22:02:38 +0200

    NOISSUE Adapt train data path

--- pl-csb-model/train/data_loader.py
+++ pl-csb-model/train/data_loader.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def load_train() -> pd.DataFrame:
-    data_path = "../pl-csb-data/data/train.tsv"
+    data_path = "../pl-csb-data/data/output/train.tsv"
     return pd.read_csv(data_path, sep="\t", index_col=0)
