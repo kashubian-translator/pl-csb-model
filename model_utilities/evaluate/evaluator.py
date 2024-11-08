6061db7b6d808f1b4a0aadbababcb69bff8a79e1
jan-Kulpas <102905054+jan-Kulpas@users.noreply.github.com>
2024-11-08 13:00:33 +0100

    Replace translation function with pipeline() and add chrF++ eval

--- model_utilities/evaluate/evaluator.py
+++ model_utilities/evaluate/evaluator.py
@@ -10,28 +10,37 @@ from translate.translator import Translator
 class ModelEvaluator:
     __logger: Logger
 
-    def __init__(self, logger) -> None:
+    def __init__(self, logger, model, tokenizer) -> None:
         self.__logger = logger
+        self.__translator = Translator(self.__logger, model, tokenizer)
 
-    def __remove_corrupted_sentences(self, sentences: pd.Series, references: pd.Series) -> tuple[pd.Series, pd.Series]:
-        df = pd.concat([sentences, references], axis=1).dropna()
-        valid_sentences, valid_references = df.iloc[:, 0], df.iloc[:, 1]
+    # def __remove_corrupted_sentences(self, sentences: pd.Series, references: pd.Series) -> tuple[pd.Series, pd.Series]:
+    #     df = pd.concat([sentences, references], axis=1).dropna()
+    #     valid_sentences, valid_references = df.iloc[:, 0], df.iloc[:, 1]
 
-        removed_sentence_count = len(sentences) - len(valid_sentences)
-        if (removed_sentence_count > 0):
-            self.__logger.info(f"Removing {removed_sentence_count} corrupted translations from evaluation dataset")
+    #     removed_sentence_count = len(sentences) - len(valid_sentences)
+    #     if (removed_sentence_count > 0):
+    #         self.__logger.info(f"Removing {removed_sentence_count} corrupted translations from evaluation dataset")
 
-        return valid_sentences, valid_references
+    #     return valid_sentences, valid_references
 
-    def evaluate(self, model: AutoModelForSeq2SeqLM, tokenizer: NllbTokenizer, sentences: pd.Series, references: pd.Series) -> sacrebleu.metrics.BLEUScore:
+    def evaluate_bleu(self, sentences: pd.Series, references: pd.Series) -> sacrebleu.metrics.BLEUScore:
         source_lang = sentences.name
         target_lang = references.name
-        translator = Translator(self.__logger, model, tokenizer, source_lang, target_lang)
 
-        translated_sentences = sentences.map(lambda s: translator.translate(s))
-        translated_sentences = translated_sentences.map(lambda s: translator.normalize_translation(s))
-
-        translated_sentences, references = self.__remove_corrupted_sentences(translated_sentences, references)
+        translated_sentences: pd.Series = sentences.map(lambda s: self.__translator.translate(s, source_lang, target_lang))
+        # translated_sentences, references = self.__remove_corrupted_sentences(translated_sentences, references)
 
         bleu = sacrebleu.corpus_bleu(translated_sentences.to_list(), references.map(lambda s: [s]).to_list())
         return bleu
+
+    def evaluate_chrf(self, sentences: pd.Series, references: pd.Series) -> sacrebleu.metrics.CHRFScore:
+        source_lang = sentences.name
+        target_lang = references.name
+
+        translated_sentences = sentences.map(lambda s: self.__translator.translate(s, source_lang, target_lang))
+        # translated_sentences, references = self.__remove_corrupted_sentences(translated_sentences, references)
+
+        # Word order = 2 means chrf++ instead of chrf
+        chrfpp = sacrebleu.corpus_chrf(translated_sentences.to_list(), references.map(lambda s: [s]).to_list(), word_order=2)
+        return chrfpp
